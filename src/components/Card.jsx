0522fc38b4b449fe76f2b853109e349821ede7c8
niba <nibachavez291@gmail.com>
2023-12-25 22:35:00 -0300

    V1.4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,19 +26,25 @@ const type            = {
     "stellar"         : "#7CC7B2"
 };
 // RENDER==================================================================================================================
-export function Card({item, onClick}) {
+export function Card({item, onClick, setLoading}) {
     // VARIABLES===========================================================================================================    
+    // USEEFFECT===========================================================================================================
+    useEffect(() => {
+        if(item.id === 151){
+            setLoading(false);
+        }
+    }, []);
     return (
-        <div className="flex flex-wrap justify-center rounded-md w-[12rem] font-bold shadow-md border font-sans md:m-14 mx-2 my-14 hover:scale-110 transition duration-300 cursor-pointer object-cover" onClick={onClick}>
+        <div className="flex flex-wrap justify-center rounded-md w-[12rem] h-[12rem] font-bold shadow-md border text-xl md:m-14 mx-2 my-14 hover:scale-110 transition duration-300 cursor-pointer object-cover" onClick={onClick}>
             <div className="h-[8rem] -mt-[7rem] items-end flex cursor-pointer">
                 <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${item.id}.gif`} alt="IMG" className="mx-auto"/>
             </div>
             <small className="w-full text-center pt-5 font-bold text-gray-400 cursor-pointer">#{item.id.toString().padStart(3, "0")}</small>
-            <label className="w-full text-center pt-5 pb-3 text-ms text-gray-500 cursor-pointer capitalize">{item.name}</label>
+            <label className="w-full text-center pt-5 pb-1 text-ms text-gray-600 cursor-pointer capitalize text-2xl">{item.name}</label>
             <div className="py-5 flex justify-center w-full cursor-pointer">
                 {item.types.map((itemType, key) => {
                     return (
-                        <span className="leading-4 px-3 py-1 rounded-full text-gray-100 capitalize font-medium mx-1 text-sm" style={{background: type[itemType.type.name]}} key={key}>{itemType.type.name}</span>
+                        <span className="leading-4 px-3 py-1 rounded-full text-gray-100 capitalize font-medium mx-1 text-md" style={{background: type[itemType.type.name]}} key={key}>{itemType.type.name}</span>
                     );
                 })}
             </div>
@@ -91,28 +97,28 @@ export function CardExpand({item}) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" className="animate-spin" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4c4.08 0 7.45 3.05 7.94 7h-4.06c-.45-1.73-2.02-3-3.88-3s-3.43 1.27-3.87 3H4.06C4.55 7.05 7.92 4 12 4" opacity=".3"/><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 2c4.08 0 7.45 3.05 7.94 7h-4.06c-.45-1.73-2.02-3-3.88-3s-3.43 1.27-3.87 3H4.06C4.55 7.05 7.92 4 12 4m2 8c0 1.1-.9 2-2 2s-2-.9-2-2s.9-2 2-2s2 .9 2 2m-2 8c-4.08 0-7.45-3.05-7.94-7h4.06c.44 1.73 2.01 3 3.87 3s3.43-1.27 3.87-3h4.06c-.47 3.95-3.84 7-7.92 7"/></svg>
                     </div>
                 :
-                    <div className="flex flex-wrap justify-center items-start rounded-md w-full font-bold shadow-md border font-sans h-[78vh]">
+                    <div className="flex flex-wrap justify-center items-start rounded-md w-full font-bold shadow-md border h-[78vh]">
                         <div className="-mt-[2rem] items-center flex h-[2rem]">
                             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${item.id}.gif`} alt="IMG" className="mx-auto scale-[3]"/>
                         </div>
                         <div className="w-full h-[65%]">
-                            <h1 className="text-center font-bold text-xl text-gray-400">#{item.id.toString().padStart(3, "0")}</h1>
-                            <h1 className="text-center py-3 text-ms capitalize text-2xl text-gray-500">{item.name}</h1>
+                            <h1 className="text-center font-bold text-gray-400 text-2xl">#{item.id.toString().padStart(3, "0")}</h1>
+                            <h1 className="text-center py-1 text-ms capitalize text-4xl text-gray-600">{item.name}</h1>
                             <div className="py-5 justify-center w-full flex">
                                 {item.types.map((itemType, key) => {
                                     return (
-                                        <span className="m-auto leading-4 px-6 py-3 rounded-full text-gray-100 capitalize font-medium mx-2 text-xl" style={{background: type[itemType.type.name]}} key={key}>{itemType.type.name}</span>
+                                        <span className="m-auto leading-4 px-6 py-2 rounded-full text-gray-100 capitalize font-medium mx-2 text-2xl" style={{background: type[itemType.type.name]}} key={key}>{itemType.type.name}</span>
                                     );
                                 })}
                             </div>
                             <div className="w-full flex flex-wrap justify-center">
-                                <label className="w-1/6 text-center text-gray-500 text-ms">{item.weight} Kg</label>
-                                <label className="w-1/6 text-center text-gray-500 text-ms">{item.height} m</label>
+                                <label className="w-1/6 text-center text-gray-600 text-2xl">{item.weight} Kg</label>
+                                <label className="w-1/6 text-center text-gray-600 text-2xl">{item.height} m</label>
                             </div>
-                            <p className="text-gray-500 px-20 py-8">
+                            <p className="text-gray-600 px-20 py-8 text-2xl">
                                 {description}
                             </p>
-                            <div className="w-full flex flex-wrap justify-center py-2 text-gray-500 uppercase font-medium">
+                            <div className="w-full flex flex-wrap justify-center text-gray-600 uppercase text-2xl">
                                 {item.stats.map((itemStats, key) => {
                                     return(
                                         <Fragment key={key}>
@@ -122,7 +128,7 @@ export function CardExpand({item}) {
                                             <div className="px-10 py-2 w-1/2">
                                                 {itemStats.base_stat}
                                             </div>
-                                            <hr className="w-[60%]"/>
+                                            {itemStats.stat.name !== "speed" && <hr className="w-[60%]"/>}                                            
                                         </Fragment>
                                     );
                                 })}
--- src/view/App.jsx
+++ src/view/App.jsx
@@ -10,51 +10,88 @@ import { Card, CardExpand }                                                     
 export default function App() {
     // VARIABLES===========================================================================================================       
     const [list, setList]                   = useState([]);
-    const [select, setSelect]               = useState({});
-    // USEEFFECT===========================================================================================================
-    useEffect(() => {
-        
-        const requestGetPokemon             = async () => {
-            try{
-                let data                    = [];
-    
-                for(let i = 1; i < 152; i++){
-                    let request             = await axios({
-                        method              : "get",
-                        url                 : `https://pokeapi.co/api/v2/pokemon/${i}`
-                    });
-    
-                    if(request.status !== 200){
-                        console.log(request);
-                        continue;
-                    }
-    
-                    data                    = [...data, request.data];
+    const [listAll, setListAll]             = useState([]);
+    const [select, setSelect]               = useState({});    
+    const [loading, setLoading]             = useState(false);
+    const requestGetPokemon                 = async () => {
+        try{
+            let data                        = [];
+
+            for(let i = 1; i < 152; i++){
+                let request                 = await axios({
+                    method                  : "get",
+                    url                     : `https://pokeapi.co/api/v2/pokemon/${i}`
+                });
+
+                if(request.status !== 200){
+                    console.log(request);
+                    continue;
                 }
 
-                setList(data);
-    
-            }catch(exception){
-                console.log(exception);
-                return;
+                data                        = [...data, request.data];
             }
-        };
 
+            setList(data);
+            setListAll(data);
+
+        }catch(exception){
+            console.log(exception);
+            return;
+        }
+    };
+    // USEEFFECT===========================================================================================================
+    useEffect(() => {
+        setLoading(true);   
         requestGetPokemon();
     }, []);
     // RENDER==============================================================================================================
     return (
-        <>            
-            <div className="container-fluid flex flex-wrap bg-white">
+        <>  
+            {loading && 
+                <div className="flex justify-center items-center h-screen animate-pulse">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" className="animate-spin" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4c4.08 0 7.45 3.05 7.94 7h-4.06c-.45-1.73-2.02-3-3.88-3s-3.43 1.27-3.87 3H4.06C4.55 7.05 7.92 4 12 4" opacity=".3"/><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 2c4.08 0 7.45 3.05 7.94 7h-4.06c-.45-1.73-2.02-3-3.88-3s-3.43 1.27-3.87 3H4.06C4.55 7.05 7.92 4 12 4m2 8c0 1.1-.9 2-2 2s-2-.9-2-2s.9-2 2-2s2 .9 2 2m-2 8c-4.08 0-7.45-3.05-7.94-7h4.06c.44 1.73 2.01 3 3.87 3s3.43-1.27 3.87-3h4.06c-.47 3.95-3.84 7-7.92 7"/></svg>
+                </div>
+            }
+            <div className={loading ? "hidden": "container-fluid flex flex-wrap bg-white font-gameboy"}>
+                <div className="flex flex-wrap justify-center md:w-[60%] pt-20 md:px-[10.6rem] w-full px-[3.2rem]">                    
+                    <input 
+                        type="text" 
+                        className="border w-full p-2 px-4 rounded-md shadow-md outline-0 focus:scale-105 transition duration-300 text-2xl"
+                        placeholder="serach by name (bulbasaur) or number (#001)"
+                        onInput={(event) => {
+                            if(event.target.value.trim() !== ""){
+                                setList(listAll.filter((item) => {
+                                    return item.name.includes(event.target.value.trim().toLowerCase());
+                                }));
+                            }else{
+                                setList(listAll);
+                            }
+                        }}
+                    />
+                </div>
+                <div className="md:w-[40%] flex flex-wrap justify-center items-center pt-14 text-7xl">
+                    POKEAPI
+                </div>
                 <div className="flex flex-wrap justify-center md:w-[60%] pt-20">
                     {list.map((item, key) => {
-                        return(<Card item={item} onClick={() => {
-                            setSelect(item);
-                        }} key={key}></Card>);
+                        return(
+                            <Card 
+                                item        = {item} 
+                                onClick     = {() => {setSelect(item)}} 
+                                key         = {key} 
+                                setLoading  = {setLoading}                               
+                            ></Card>
+                        );
                     })}
                 </div>
                 <div className="md:w-[40%] h-screen sticky top-0 p-10 pt-40">
-                    {select.id && <CardExpand item={select}></CardExpand>}                    
+                    {select.id ? 
+                        <CardExpand item={select}></CardExpand>
+                        :
+                        <div className="flex justify-center items-center h-[70vh]">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4c4.08 0 7.45 3.05 7.94 7h-4.06c-.45-1.73-2.02-3-3.88-3s-3.43 1.27-3.87 3H4.06C4.55 7.05 7.92 4 12 4" opacity=".3"/><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 2c4.08 0 7.45 3.05 7.94 7h-4.06c-.45-1.73-2.02-3-3.88-3s-3.43 1.27-3.87 3H4.06C4.55 7.05 7.92 4 12 4m2 8c0 1.1-.9 2-2 2s-2-.9-2-2s.9-2 2-2s2 .9 2 2m-2 8c-4.08 0-7.45-3.05-7.94-7h4.06c.44 1.73 2.01 3 3.87 3s3.43-1.27 3.87-3h4.06c-.47 3.95-3.84 7-7.92 7"/></svg>
+                        </div>
+                    }
                 </div>
             </div>
         </>
